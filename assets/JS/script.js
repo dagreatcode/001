24249a206cbef6ecd4ea1df392f278e734c70fed
Vincent Kendrick <dagreatcode@gmail.com>
2022-01-26 07:49:22 -0500

    javascript practice2

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,5 +3,11 @@ var price = 100;
 var isCustomer = true;
 
 alert("Welcome: " + nameFirst);
+var sitePlease = confirm("Do you want a website for " + price + " US$?");
+console.log("Hello " + nameFirst);
+console.log("The price for these sites go for " + price + "a site.");
 
-console.log("Hello World");
+if (sitePlease){
+    alert("Great");
+} else
+    alert("See You Later");
