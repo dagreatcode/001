d245157c5d0bee4c230df040741d9b86a7119e28
Vincent Kendrick <dagreatcode@gmail.com>
2022-01-26 08:32:11 -0500

    javascript for Loops and Conditions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,22 @@
-var nameFirst = "User";
 var price = 100;
 var isCustomer = true;
 
+// Is this array sorted?
+var arrayPractice1 = [1,7,9,9,4,5];
+var arrayPractice2 = ["Vincent", "Vincent Jr.", "Voughn", "Barbara"];
+
+var nameFirst = prompt("What is your name?");
 alert("Welcome: " + nameFirst);
 var sitePlease = confirm("Do you want a website for " + price + " US$?");
 console.log("Hello " + nameFirst);
-console.log("The price for these sites go for " + price + "a site.");
+console.log("The price for these sites go for " + price + "$ per site.");
+
+for (var i = 0; i < arrayPractice2.length; i++) {
+    console.log(arrayPractice2[i]);
+}
 
 if (sitePlease){
-    alert("Great");
-} else
-    alert("See You Later");
+    alert("Great " + nameFirst);
+} else {
+    alert("See You Later " + nameFirst);
+}
